0117a988692c95fe3f65ea634a4e089478d9d575
Qbaldo <durowski@gmail.com>
2026-07-23 00:26:25 +0200

    Add Worker model with Position relation +settings.py with AUTH_USER_MODEL

--- IT_task_manager/models.py
+++ IT_task_manager/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Position(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +9,10 @@ class Position(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.position.name} {self.first_name} {self.last_name}"
