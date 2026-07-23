b0aec3652905a72ce08d2913068ca07983286714
Qbaldo <durowski@gmail.com>
2026-07-24 00:07:35 +0200

    Add migrations | Add task and task type models

--- IT_task_manager/models.py
+++ IT_task_manager/models.py
@@ -16,3 +16,30 @@ class Worker(AbstractUser):
 
     def __str__(self):
         return f"{self.position.name} {self.first_name} {self.last_name}"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    class Priority(models.IntegerChoices):
+        LOW = 1
+        MEDIUM = 2
+        HIGH = 3
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.IntegerField(choices=Priority.choices, default=Priority.MEDIUM)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker)
+    class Meta:
+        ordering = ["name"]
+    def __str__(self):
+        return f"{self.task_type.name} {self.name}"
--- IT_task_manager/views.py
+++ IT_task_manager/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Position, Worker
+
 def position_list(request):
     positions = Position.objects.all()
     return render(request, 'task_manager/position_list.html', {'positions': positions})
+
 def worker_list(request):
     workers = Worker.objects.all()
     return render(request, 'task_manager/worker_list.html', {'workers': workers})
