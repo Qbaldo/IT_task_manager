14c1d985499134bf8e280443459711d87ccf0bd2
Qbaldo <durowski@gmail.com>
2026-07-23 23:29:10 +0200

    Add base template and position/worker list views

--- IT_task_manager/settings.py
+++ IT_task_manager/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'IT_task_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
--- IT_task_manager/urls.py
+++ IT_task_manager/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from views import position_list, worker_list
+from IT_task_manager.views import position_list, worker_list
 
 
 urlpatterns = [
